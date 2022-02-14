b46e8a887bf93e5a39c98122224742bcbdabc700
Michael Hinshaw <michaelhinshaw@gmail.com>
2022-02-13 22:37:57 -0800

    add babel to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const classicCadence = {
-  1: Ab,
-  4: Db,
-  5: Eb
+  1: "Ab",
+  4: "Db",
+  5: "Eb"
+};
+
+const classicCadenceSubs = {
+  ...classicCadence,
+  6: "Fm",
+  "5a": "A",
+  5: "Eb"
 };
 
 console.log(classicCadence);
+console.log(classicCadenceSubs);
